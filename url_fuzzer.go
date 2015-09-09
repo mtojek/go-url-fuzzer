513cb5ef57d7f885ebddfd49b9a57dd51eae4086
Marcin Tojek <tojek.m@gmail.com>
2015-09-09 23:29:32 +0200

    Usage

--- url_fuzzer.go
+++ url_fuzzer.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	headers            = kingpin.Flag("header", "Header=value, custom HTTP header added to every fuzz request").Short('h').StringMap()
+	methods            = kingpin.Flag("method", "HTTP method used in tests (GET, POST, PUT, DELETE, HEAD, OPTIONS)").Short('m').Default("GET").Strings()
+	outputFile         = kingpin.Flag("output", "Output text file with found urls and statuses").Short('o').String()
+	reportDirectory    = kingpin.Flag("report", "Target output directory of an url fuzzing HTML report").Short('r').File()
+	urlResponseTimeout = kingpin.Flag("timeout", "Fuzzed url response timeout, in milliseconds").Short('t').Default("5000").Uint64()
+	workerWaitPeriod   = kingpin.Flag("wait-period", "Time wait period between fuzz tests per worker, in milliseconds").Short('w').Default("0").Uint64()
+	fuzzSetFile        = kingpin.Arg("fuzz-set-file", "File containing fuzz entry set, one entry per line").Required().ExistingFile()
+	baseURL            = kingpin.Arg("base-url", "Number of packets to send").Required().URL()
+)
 
 func main() {
-	fmt.Println("1")
+	kingpin.UsageTemplate(kingpin.CompactUsageTemplate).Version("0.1").Author("Marcin Tojek")
+	kingpin.Parse()
 }
