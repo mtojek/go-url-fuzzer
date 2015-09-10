6d649fff20288c00a3c00ef58f576312be5b19ae
Marcin Tojek <tojek.m@gmail.com>
2015-09-10 23:36:36 +0200

    Working on configuration from command line

---
+++ configuration/configuration.go
@@ -0,0 +1,21 @@
+package configuration
+
+import (
+	"net/url"
+	"time"
+)
+
+type Configuration struct {
+	Headers            map[string]string
+	Methods            []string
+	OutputFile         string
+	ReportDirectory    string
+	UrlResponseTimeout time.Duration
+	WorkerWaitPeriod   time.Duration
+	FuzzSetFile        string
+	BaseURL            url.URL
+}
+
+func NewConfiguration() *Configuration {
+	return new(Configuration)
+}
