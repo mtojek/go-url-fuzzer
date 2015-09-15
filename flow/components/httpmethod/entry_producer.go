57f8bf57a1ae329720ecb10e6dd029f3c2d13dea
Marcin Tojek <tojek.m@gmail.com>
2015-09-15 21:31:34 +0200

    Entry producer struct

--- flow/components/httpmethod/entry_producer.go
+++ flow/components/httpmethod/entry_producer.go
@@ -1,1 +1,11 @@
 package httpmethod
+
+import "github.com/trustmaster/goflow"
+
+// EntryProducer processes relative URLs and
+type EntryProducer struct {
+	flow.Component
+
+	RelativeURL <-chan string
+	Entry       chan<- Entry
+}
