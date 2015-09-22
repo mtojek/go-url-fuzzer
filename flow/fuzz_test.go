ae42dbbc027c6bd1cb7b3ac13c437cba8979035a
Marcin Tojek <tojek.m@gmail.com>
2015-09-22 20:43:08 +0200

    Fuzz minimal configuration test

--- flow/fuzz_test.go
+++ flow/fuzz_test.go
@@ -1,1 +1,28 @@
 package flow
+
+import (
+	"testing"
+
+	"github.com/mtojek/go-url-fuzzer/configuration"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewFuzzMinimalConfiguration(t *testing.T) {
+	assert := assert.New(t)
+
+	// given
+	var workersNumber uint64 = 2
+	methods := []string{"GET", "POST"}
+
+	builder := configuration.NewBuilder()
+	configuration := builder.
+		WorkersNumber(workersNumber).
+		Methods(methods).
+		Build()
+
+	// when
+	sut := NewFuzz(configuration)
+
+	// then
+	assert.NotNil(sut, "Instance should be created")
+}
