e3ab0e1c1132041a76c87607f1d33ff9c88e2c0a
Marcin Tojek <tojek.m@gmail.com>
2015-09-22 22:15:44 +0200

    Sample fuzz test

--- flow/fuzz_test.go
+++ flow/fuzz_test.go
@@ -3,6 +3,9 @@ package flow
 import (
 	"testing"
 
+	"log"
+	"os"
+
 	"github.com/mtojek/go-url-fuzzer/configuration"
 	"github.com/stretchr/testify/assert"
 )
@@ -25,6 +28,33 @@ func TestNewFuzzMinimalConfiguration(t *testing.T) {
 
 	// then
 	assert.NotNil(sut, "Instance should be created")
+	assert.NotNil(sut.input, "Input channel should be defined")
 	assert.NotNil(sut.graph, "Flow graph should be created")
 	assert.NotNil(sut.configuration, "Configuration should be set")
+}
+
+func TestStartFuzz(t *testing.T) {
+	assert := assert.New(t)
+
+	// given
+	var workersNumber uint64 = 2
+	methods := []string{"GET", "POST", "PUT"}
+	inputFile, error := os.OpenFile("../resources/input-data/fuzz_03.txt", os.O_RDONLY, 0666)
+	if nil != error {
+		log.Fatal("TestStartFuzz: ", error)
+	}
+
+	builder := configuration.NewBuilder()
+	configuration := builder.
+		WorkersNumber(workersNumber).
+		Methods(methods).
+		FuzzSetFile(inputFile).
+		Build()
+	sut := NewFuzz(configuration)
+
+	// when
+	sut.Start()
+
+	// then
+	assert.Len(sut.input, 0, "Input channel should be empty now")
 }
